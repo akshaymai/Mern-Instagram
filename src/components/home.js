be8a6af9c312c0c835fec6b2b12f9332a6c534d2
akshay <akshay.maity76@gmail.com>
2020-06-05 01:08:03 +0530

    home page add

--- src/components/home.js
+++ src/components/home.js
@@ -3,10 +3,55 @@ import React, { Component } from 'react';
 
 const Home = (props) => {
     return (
-    <div>
-        <h1>Home</h1>
-        
-    </div>
-    );
+ <div className="home">
+     <div className="card home-card">
+         <h5>akshay</h5>
+         <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+         </div>  
+         <div class="card-content">
+             
+  <i className=" material-icons " style={{color:'red'}}>favorite</i>
+           <h6>akshay maity</h6>
+           <h1>This is my post</h1>
+           <input type="text"  placeholder="enter comment"/>
+        </div>
+           
+     </div>
+     <div className="card home-card">
+         <h5>akshay</h5>
+         <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+         </div>  
+         <div class="card-content">
+             
+  <i className=" material-icons " style={{color:'red'}}>favorite</i>
+           <h6>akshay maity</h6>
+           <h1>This is my post</h1>
+           <input type="text"  placeholder="enter comment"/>
+
+
+        </div>
+           
+     </div>
+     <div className="card home-card">
+         <h5>akshay</h5>
+         <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+         </div>  
+         <div class="card-content">
+             
+  <i className=" material-icons " style={{color:'red'}}>favorite</i>
+           <h6>akshay maity</h6>
+           <h1>This is my post</h1>
+           <input type="text"  placeholder="enter comment"/>
+
+
+        </div>
+           
+     </div>
+      
+ </div>
+)
 }
 export default Home;
