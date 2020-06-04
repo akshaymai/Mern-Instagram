a86410fa0a92e4f14ba82d293f36c188b344bbff
akshay <akshay.maity76@gmail.com>
2020-06-05 01:45:48 +0530

    profile add

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,10 +3,40 @@ import React, { Component } from 'react';
 
 const Profile = (props) => {
     return (
-    <div>
-        <h1>Profile</h1>
+    <div style={{maxWidth:"950px" ,margin:"0px auto"}}>
+         <div style={{
+             justifyContent:"space-around",
+             display:"flex",
+             borderBottom:"1px solid gray",
+             margin:"18px 0px"
+         }}>
+       
+         <div>
+
+         <img 
+         src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+         style={{borderRadius:"80px",height:"160px",width:"160px"}}/>
+         </div> 
+         <div>   
+         <h4>akshay maity</h4>
+         <div style={{justifyContent:"space-around",display:"flex",width:"108%"}}>
+             <h6>20 post</h6>
+             <h6>20 post</h6>
+             <h6>20 post</h6>
+             </div>
+             </div>
+         </div>  
+
+
+         <div className="gallery">
+
+         <img className="item" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/> 
+         <img className="item" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/> 
+         <img className="item" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/> 
         
-    </div>
+        <img className="item" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/> 
+         </div> 
+     </div>
     );
 }
 export default Profile;
